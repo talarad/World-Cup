ef2455cb9329b174eacee69e2ff07bf23f21e458
Tal Arad <talarad5@gmail.com>
2018-05-13 00:50:00 +0300

    'test'

--- react-backend/app.js
+++ react-backend/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var indexRouter = require('./routes/site');
 var index = require('./routes/index');
 var users = require('./routes/users');
