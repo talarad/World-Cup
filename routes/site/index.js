5e22e8d0fc8b7c456da4337811e197a935576edf
Tal Arad <talarad5@gmail.com>
2018-07-15 13:45:28 +0300

    updated score for win

--- routes/site/index.js
+++ routes/site/index.js
@@ -553,11 +553,11 @@ function updatePoints(game) {
 
     if (bet.id === game.id) {
       if (bet.home.toString() === game.home.toString() && bet.away.toString() === game.away.toString()) {
-        betUser.score += 90;
+        betUser.score += 100;
       } else if ((bet.home.toString() > bet.away.toString() && game.home.toString() > game.away.toString()) ||
         (bet.home.toString() < bet.away.toString() && game.home.toString() < game.away.toString()) ||
         (bet.home.toString() === bet.away.toString() && game.home.toString() === game.away.toString())) {
-        betUser.score += 30;
+        betUser.score += 40;
       }
     }
   })
