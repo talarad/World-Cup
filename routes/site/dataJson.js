b29a36341dabce5c030b4f02369617a6fe2689a8
Tal <talarad5@gmail.com>
2022-11-24 21:48:17 +0200

    more matches

--- routes/site/dataJson.js
+++ routes/site/dataJson.js
@@ -196,6 +196,291 @@ const data = [
         "home_id": "31",
         "away_id": "32"
     },
+    {
+        "id": "17",
+        "date": "2022-11-25",
+        "time": "12:00:00",
+        "home_name": "Iran",
+        "away_name": "Wales",
+        "league_id": "793",
+        "home_id": "4",
+        "away_id": "8"
+    },
+    {
+        "id": "18",
+        "date": "2022-11-25",
+        "time": "15:00:00",
+        "home_name": "Senegal",
+        "away_name": "Qatar",
+        "league_id": "793",
+        "home_id": "5",
+        "away_id": "1"
+    },
+    {
+        "id": "19",
+        "date": "2022-11-25",
+        "time": "18:00:00",
+        "home_name": "Netherlands",
+        "away_name": "Ecuador",
+        "league_id": "793",
+        "home_id": "6",
+        "away_id": "2"
+    },
+    {
+        "id": "20",
+        "date": "2022-11-25",
+        "time": "21:00:00",
+        "home_name": "England",
+        "away_name": "United States",
+        "league_id": "793",
+        "home_id": "3",
+        "away_id": "7"
+    },
+    {
+        "id": "21",
+        "date": "2022-11-26",
+        "time": "12:00:00",
+        "home_name": "Australia",
+        "away_name": "Tunisia",
+        "league_id": "793",
+        "home_id": "16",
+        "away_id": "12"
+    },
+    {
+        "id": "22",
+        "date": "2022-11-26",
+        "time": "15:00:00",
+        "home_name": "Saudi Arabia",
+        "away_name": "Poland",
+        "league_id": "793",
+        "home_id": "10",
+        "away_id": "14"
+    },
+    {
+        "id": "23",
+        "date": "2022-11-26",
+        "time": "18:00:00",
+        "home_name": "France",
+        "away_name": "Denmark",
+        "league_id": "793",
+        "home_id": "15",
+        "away_id": "11"
+    },
+    {
+        "id": "24",
+        "date": "2022-11-26",
+        "time": "21:00:00",
+        "home_name": "Mexico",
+        "away_name": "Argentina",
+        "league_id": "793",
+        "home_id": "13",
+        "away_id": "9"
+    },
+    {
+        "id": "25",
+        "date": "2022-11-27",
+        "time": "12:00:00",
+        "home_name": "Japan",
+        "away_name": "Costa Rica",
+        "league_id": "793",
+        "home_id": "20",
+        "away_id": "22"
+    },
+    {
+        "id": "26",
+        "date": "2022-11-27",
+        "time": "15:00:00",
+        "home_name": "Belgium",
+        "away_name": "Morocco",
+        "league_id": "793",
+        "home_id": "23",
+        "away_id": "17"
+    },
+    {
+        "id": "27",
+        "date": "2022-11-27",
+        "time": "18:00:00",
+        "home_name": "Croatia",
+        "away_name": "Canada",
+        "league_id": "793",
+        "home_id": "18",
+        "away_id": "24"
+    },
+    {
+        "id": "28",
+        "date": "2022-11-27",
+        "time": "21:00:00",
+        "home_name": "Spain",
+        "away_name": "Germany",
+        "league_id": "793",
+        "home_id": "21",
+        "away_id": "19"
+    },
+    {
+        "id": "29",
+        "date": "2022-11-28",
+        "time": "12:00:00",
+        "home_name": "Serbia",
+        "away_name": "Cameroon",
+        "league_id": "793",
+        "home_id": "32",
+        "away_id": "26"
+    },
+    {
+        "id": "30",
+        "date": "2022-11-28",
+        "time": "15:00:00",
+        "home_name": "Ghana",
+        "away_name": "Korea Republic",
+        "league_id": "793",
+        "home_id": "30",
+        "away_id": "28"
+    },
+    {
+        "id": "31",
+        "date": "2022-11-28",
+        "time": "18:00:00",
+        "home_name": "Brazil",
+        "away_name": "Switzerland",
+        "league_id": "793",
+        "home_id": "31",
+        "away_id": "25"
+    },
+    {
+        "id": "32",
+        "date": "2022-11-28",
+        "time": "21:00:00",
+        "home_name": "Portugal",
+        "away_name": "Uruguay",
+        "league_id": "793",
+        "home_id": "29",
+        "away_id": "27"
+    },
+    {
+        "id": "33",
+        "date": "2022-11-29",
+        "time": "17:00:00",
+        "home_name": "Netherlands",
+        "away_name": "Qatar",
+        "league_id": "793",
+        "home_id": "6",
+        "away_id": "1"
+    },
+    {
+        "id": "34",
+        "date": "2022-11-29",
+        "time": "17:00:00",
+        "home_name": "Senegal",
+        "away_name": "Ecuador",
+        "league_id": "793",
+        "home_id": "5",
+        "away_id": "2"
+    },
+    {
+        "id": "35",
+        "date": "2022-11-29",
+        "time": "21:00:00",
+        "home_name": "Iran",
+        "away_name": "United States",
+        "league_id": "793",
+        "home_id": "4",
+        "away_id": "7"
+    },
+    {
+        "id": "36",
+        "date": "2022-11-29",
+        "time": "21:00:00",
+        "home_name": "England",
+        "away_name": "Wales",
+        "league_id": "793",
+        "home_id": "3",
+        "away_id": "8"
+    },
+    {
+        "id": "37",
+        "date": "2022-11-30",
+        "time": "17:00:00",
+        "home_name": "Australia",
+        "away_name": "Denmark",
+        "league_id": "793",
+        "home_id": "16",
+        "away_id": "11"
+    },
+    {
+        "id": "38",
+        "date": "2022-11-30",
+        "time": "17:00:00",
+        "home_name": "Tunisia",
+        "away_name": "France",
+        "league_id": "793",
+        "home_id": "12",
+        "away_id": "15"
+    },
+    {
+        "id": "39",
+        "date": "2022-11-30",
+        "time": "21:00:00",
+        "home_name": "Poland",
+        "away_name": "Argentina",
+        "league_id": "793",
+        "home_id": "14",
+        "away_id": "9"
+    },
+    {
+        "id": "40",
+        "date": "2022-11-30",
+        "time": "21:00:00",
+        "home_name": "Mexico",
+        "away_name": "Saudi Arabia",
+        "league_id": "793",
+        "home_id": "13",
+        "away_id": "10"
+    },
+
+
+    {
+        "id": "41",
+        "date": "2022-12-01",
+        "time": "17:00:00",
+        "home_name": "Belgium",
+        "away_name": "Croatia",
+        "league_id": "793",
+        "home_id": "23",
+        "away_id": "18"
+    },
+    {
+        "id": "42",
+        "date": "2022-12-01",
+        "time": "17:00:00",
+        "home_name": "Morocco",
+        "away_name": "Canada",
+        "league_id": "793",
+        "home_id": "17",
+        "away_id": "24"
+    },
+    {
+        "id": "43",
+        "date": "2022-12-01",
+        "time": "21:00:00",
+        "home_name": "Japan",
+        "away_name": "Spain",
+        "league_id": "793",
+        "home_id": "20",
+        "away_id": "21"
+    },
+    {
+        "id": "44",
+        "date": "2022-12-01",
+        "time": "21:00:00",
+        "home_name": "Germany",
+        "away_name": "Costa Rica",
+        "league_id": "793",
+        "home_id": "19",
+        "away_id": "22"
+    },
 ]
 
 module.exports = data;
+
+
+
