a74ba40ec079cf870028b6051ec7b8b15638e54e
Tal <talarad5@gmail.com>
2022-12-07 13:55:27 +0200

    Changed score

--- routes/site/index.js
+++ routes/site/index.js
@@ -553,11 +553,11 @@ function updatePoints(game) {
 
     if (bet.id === game.id) {
       if (bet.home.toString() === game.home.toString() && bet.away.toString() === game.away.toString()) {
-        betUser.score += 30;
+        betUser.score += 50;
       } else if ((bet.home.toString() > bet.away.toString() && game.home.toString() > game.away.toString()) ||
         (bet.home.toString() < bet.away.toString() && game.home.toString() < game.away.toString()) ||
         (bet.home.toString() === bet.away.toString() && game.home.toString() === game.away.toString())) {
-        betUser.score += 10;
+        betUser.score += 20;
       }
     }
   })
